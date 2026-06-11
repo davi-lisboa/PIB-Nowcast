8c410a6fd69f41932b9b4209621d873295bcf18e
Davi Lisboa <lisboa.davi1703@gmail.com>
2026-06-11 20:14:38 +0000

    add series_spec, update get_* functions

--- collect_data.py
+++ collect_data.py
@@ -2,70 +2,51 @@
 from tenacity import wait_fixed, retry, stop_after_attempt
 
 @retry(stop=stop_after_attempt(5))
-def get_bcb(serie:dict, start:str|None = None):
+def get_bcb(serie:dict|None = None, start:str|None = None):
+
+    import pandas as pd
     from bcb import sgs
-    return sgs.get(serie, start)
 
+    series = pd.read_csv('series_spec.csv', sep=';') \
+                .query("variable != 'pib'") \
+                .astype({'sgs_code': int})
 
-series = {
-                  # Output
-                  'ind_extrativa':28504,
-                  'ind_transformacao':28505,
-                  'ind_bens_capital':28506,
-                  'ind_bens_intermediarios':28507,
-                  'ind_bens_consumo':28508,
-                  'ind_construcao':28511,
+    series = series.set_index('variable')['sgs_code'].to_dict()
+    
+    return sgs.get(series, start)
 
-                  'cons_energ_comercial':1402,
-                  'cons_energ_indusrial':1404,
+get_bcb()
 
-                  'pmc_ampliada': 28485,
-                  'ibcbr_agro':29602,
-                  'ibcbr_ind':29604,
-                  'ibcbr_servicos':29606,
-
-                  # Sentiment
-                  'icc_fecomercio':4393,
-                  'icea_fecomercio':4394,
-                  'ief_fecomercio':4395,
-
-                  'ics_fgv':20339,
-                  'isas_fgv':20340,
-                  'ies_fgv':20341,
-
-                  # Employment
-                  'caged_estoque':28784,
-                  'renda_media':24399,
-                  'tx_desemprego':24369,
-
-                  # Prices
-                  'ipca_12m':13522,
-                  'ipca15': 7478,
-
-                  'igpm':189,
-                  'igpdi':190,
-                  'incc':192,
-                  'ipam':7450
-              }
-
-
-get_bcb(series)
 # %% get_pib
 
-
+@retry(stop=stop_after_attempt(5))
 def get_pib():
-    
+    import pandas as pd
     import sidrapy
     # https://apisidra.ibge.gov.br/values/t/1620/n1/all/v/all/p/all/c11255/90707/d/v583%202
 
-
-    return sidrapy.get_table(
+    pib = sidrapy.get_table(
                                 table_code='1620', 
                                 territorial_level='1', 
                                 ibge_territorial_code='all', 
-                                period='all'
+                                period='all',
                                 variable='all', 
                                 classification='11255/90707', 
                             )
+    
+    pib = pib[['D2C', 'V']].iloc[1:].reset_index(drop=True)
+    pib.columns = ['Date', 'pib']
+    pib = pib.assign(
+                        year = pib['Date'].str[:4],
+                        month = pib['Date'].str[-2:].astype(int).multiply(3).astype(str).str.zfill(2),
+                        Date = lambda df: df['year'] + df['month'],
+                    ) \
+            .assign(
+                        Date = lambda df: pd.to_datetime(df['Date'], format='%Y%m')
+                    ) \
+            .drop(columns=['year', 'month']) \
+            .set_index('Date')
 
-get_pib()
+    return pib 
+
+# %%
