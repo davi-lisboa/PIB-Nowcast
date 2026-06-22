ea763ea5b6cc378bbe5dc421ff56f3f139e30c2d
Davi Lisboa <lisboa.davi1703@gmail.com>
2026-06-22 17:37:26 +0000

    Fix path issues, i think...

--- src/pib_nowcast/workflow/testing.py
+++ src/pib_nowcast/workflow/testing.py
@@ -1,4 +1,11 @@
 # %% Libs
 import os
 from pathlib import Path
-from src.pib_nowcast.utils.get_data import get_bcb
+
+import pandas as pd
+import numpy as np
+import datetime as dt
+
+from pib_nowcast.utils.get_data import get_bcb, get_pib, get_ipeadata
+
+# %%
